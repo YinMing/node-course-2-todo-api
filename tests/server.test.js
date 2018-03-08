76cd17d1902e239f4d5ea6d03510942917dfc500
YinMing <apluskei@gmail.com>
2018-03-08 23:00:36 +0800

    Test the DELETE /todos/:id route

--- tests/server.test.js
+++ tests/server.test.js
@@ -118,3 +118,51 @@ describe('GET /todos/:id', ()=>
     });
 
 });
+
+
+describe('DELETE /todos/:id', ()=>
+{
+  it('should remove a todo', (done)=>
+  {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res)=>
+      {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res)=>
+      {
+          if(err) return done(err);
+
+          //query db using findById toNotExist
+          //expect(null).toNotExist();
+          Todo.findById(hexId).then((doc)=>
+          {
+            expect(doc).toNotExist();
+            done();
+          }).catch((e)=>done(e));
+      });
+  });
+
+  it('should retur 404 if todo not found', (done)=>
+  {
+      //make sure you get a 404 back
+      var hexId = new ObjectID().toHexString();
+      request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(404)
+        .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done)=>
+  {
+    //tods/123
+    request(app)
+      .delete('/todos/123abc')
+      .expect(404)
+      .end(done);
+  });
+
+});
